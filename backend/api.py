b4fa987a27819caef97e7c862d808cc507265d10
smilinglama <dnarasimhan@clari.com>
2020-12-08 22:32:55 -0800

    login changes

--- backend/api.py
+++ backend/api.py
@@ -1,12 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import (LoginManager,
+                         login_user, logout_user,
+                         login_required, UserMixin, current_user)
+
 from sqlalchemy import Integer, String, Date, Boolean, Float, DateTime
 import user_route
 import relationship_route
 import event_route
-    
+
 app1 = Flask(__name__)
 
 cors = CORS(app1)
@@ -21,12 +25,18 @@ db1.init_app(app1)
 migrate = Migrate(app1, db1)
 
 
-class User(db1.Model):
+#login related
+app1.config['SECRET_KEY'] = 'secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app1)
+
+class User(UserMixin, db1.Model):
     __tablename__ = 'user'
     id = db1.Column(Integer, primary_key=True)
     firstName = db1.Column(String)
     lastName = db1.Column(String)
     email = db1.Column(String)
+    password = db1.Column(String)
     address = db1.Column(String)
     age = db1.Column(Integer)
     testResult = db1.Column(String, nullable=True)
@@ -34,32 +44,29 @@ class User(db1.Model):
     hasSymptoms = db1.Column(Boolean, nullable=True)
     symptomsOnSetDate = db1.Column(Date, nullable=True)
 
-    def __init__(self, firstName, lastName, email, address, age):
+    def __init__(self, firstName, lastName, email, password, address, age):
         self.firstName = firstName
         self.lastName = lastName
         self.email = email
+        self.password = password
         self.address = address
         self.age = age
 
     def __repr__(self):
         return f"<User {self.name}>"
 
+    def to_json(self):
+        return {"name": self.firstName,
+                "email": self.email}
 
-class Relationship(db1.Model):
-    __tablename__ = 'relationship'
-    id = db1.Column(Integer, primary_key=True)
-    userId = db1.Column(Integer)
-    friendId = db1.Column(Integer)
-    relationshipType = db1.Column(String, nullable=True)
+    def is_authenticated(self):
+        return True
 
-    def __init__(self, userId, friendId, relationshipType):
-        self.userId = userId 
-        self.friendId = friendId
-        self.relationshipType = relationshipType
+    def is_active(self):
+        return True
 
-    def __repr__(self):
-        return f"<Relationship {self.userId} {self.friendId} {self.relationshipType}>"
-
+    def is_anonymous(self):
+        return False
 
 class Event(db1.Model):
     __tablename__ = 'event'
@@ -77,7 +84,7 @@ class Event(db1.Model):
     updatedDate = db1.Column(DateTime)
 
     def __init__(self, placeName, address, numPeople, socialDistanceRating, maskComplianceRating, openSpace, riskScore, createdById, checkInDate, checkOutDate, updatedDate):
-        self.placeName = placeName 
+        self.placeName = placeName
         self.address = address
         self.numPeople = numPeople
         self.socialDistanceRating = socialDistanceRating
@@ -96,36 +103,55 @@ class Event(db1.Model):
 def hello_world():
     return 'Hello, World!'
 
-# @app.route('/signup', methods=['POST'])
-# def signup():
-#     if request.method == 'POST':
-#         return perform_signup()
-#
-# @app.route('/login', methods=['POST'])
-# def login():
-#     if request.method == 'POST':
-#         return perform_login()
-#
-# @app.route('/getUserData', methods=['GET'])
-# def getUserData():
-#     if request.method == 'GET':
-#         return perform_login()
+@app1.route('/login', methods=['POST'])
+def login():
+    if request.is_json:
+        data = request.get_json()
+        email = data['username']
+        password = data['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+           login_user(user)
+           return jsonify(user.to_json())
+        else:
+           return jsonify({"status": 401,
+                        "reason": "Username or Password is Invalid"})
+
+@app1.route('/logout', methods=['GET'])
+def logout():
+    logout_user()
+    return jsonify(**{'result': 200,
+                      'data': {'message': 'User is successfully logged out'}})
+
+@app1.route('/user_info', methods=['GET'])
+def user_info():
+    if current_user.is_authenticated:
+        resp = {"result": 200,
+                "message": current_user.to_json()}
+    else:
+        resp = {"result": 401,
+                "message": "Log in to access User info"}
+    return jsonify(**resp)
 
 @app1.route('/signup', methods=['POST'])
 def handle_user():
-    try: 
-        if request.method == 'POST':
-            if request.is_json:
-                data = request.get_json()
-                user = User(firstName=data['firstName'], lastName=data['lastName'], email=data['email'],
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            user = User(firstName=data['firstName'], lastName=data['lastName'], email=data['email'],
                         address=data['address'], age=data['age'])
-                db1.session.add(user)
-                db1.session.commit()
-                return {"message": f"User {user.firstName} has been created successfully."}
-            else:
-                return {"error": "The request payload is not in JSON format"}
-    except Exception as e:
-        print(e)
+            db1.session.add(user)
+            db1.session.commit()
+            return {"message": f"User {user.firstName} has been created successfully."}
+        else:
+            return {"error": "The request payload is not in JSON format"}
+    else:
+        return {"error": "The request Method is not valid. Expecting a POST Request"}
+
  
 if __name__ == '__main__':
     app1.run(debug=True)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(email=user_id).first()
