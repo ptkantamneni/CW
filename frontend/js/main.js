ab2d2a132ea52ba737353fc98ea7d5b7879a3d32
Aditya Varkhedi <avarkhedi@clari.com>
2020-12-08 19:07:53 -0800

    Login/Signup frontend, plus jquery ajax setup.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,6 +2,37 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    
+	$("#login-button").click(function(){
+		$.ajax({
+			'url' : 'http://localhost:5000/login',
+			'type' : 'POST',
+			'data' : {
+			    'numberOfWords' : 10
+			},
+			'success' : function(data) {              
+			    alert('Data: '+data);
+			},
+			'error' : function(request,error)
+			{
+			    alert("Request: "+JSON.stringify(request));
+			}
+		});    
+	});
 
+	$("#signup-button").click(function(){
+		$.ajax({
+			'url' : 'http://localhost:5000/signup',
+			'type' : 'POST',
+			'data' : {
+			    'numberOfWords' : 10
+			},
+			'success' : function(data) {              
+			    alert('Data: '+data);
+			},
+			'error' : function(request,error)
+			{
+			    alert("Request: "+JSON.stringify(request));
+			}
+		});    
+	});
 });
