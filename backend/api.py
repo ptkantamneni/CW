a596539b3e4e59c1af905792a1b3bc872b4bc40b
David Nguyen <dnguyen@clari.com>
2020-12-08 23:35:17 -0800

    added back models to api.py, added relationship routes

--- backend/api.py
+++ backend/api.py
@@ -67,6 +67,58 @@ class User(UserMixin, db1.Model):
     def get_id(self):
         return str(self.email)
 
+class Relationship(db1.Model):
+    __tablename__ = 'relationship'
+    id = db1.Column(Integer, primary_key=True)
+    userId = db1.Column(Integer)
+    friendId = db1.Column(Integer)
+    relationshipType = db1.Column(String, nullable=True)
+
+    def __init__(self, userId, friendId, relationshipType):
+        self.userId = userId
+        self.friendId = friendId
+        self.relationshipType = relationshipType
+
+    def __repr__(self):
+        return f"<Relationship {self.userId} {self.friendId} {self.relationshipType}>"
+
+    def to_json(self):
+        return {"userId": self.userId,
+                "friendId": self.friendId,
+                "relationshipType": self.relationshipType}
+
+class Event(db1.Model):
+    __tablename__ = 'event'
+    id = db1.Column(Integer, primary_key=True)
+    placeName = db1.Column(String)
+    address = db1.Column(String)
+    numPeople = db1.Column(Integer)
+    socialDistanceRating = db1.Column(Integer)
+    maskComplianceRating = db1.Column(Integer)
+    openSpace = db1.Column(Boolean)
+    riskScore = db1.Column(Float)
+    createdById = db1.Column(Integer)
+    checkInDate = db1.Column(DateTime)
+    checkOutDate = db1.Column(DateTime)
+    updatedDate = db1.Column(DateTime)
+
+    def __init__(self, placeName, address, numPeople, socialDistanceRating, maskComplianceRating, openSpace, riskScore, createdById, checkInDate, checkOutDate, updatedDate):
+        self.placeName = placeName
+        self.address = address
+        self.numPeople = numPeople
+        self.socialDistanceRating = socialDistanceRating
+        self.maskComplianceRating = maskComplianceRating
+        self.openSpace = openSpace
+        self.riskScore = riskScore
+        self.createdById = createdById
+        self.checkInDate = checkInDate
+        self.checkOutDate = checkOutDate
+        self.updatedDate = updatedDate
+
+    def __repr__(self):
+        return f"<Event {self.placeName} {self.address} {self.numPeople}>"
+
+
 @app1.route('/')
 def hello_world():
     return 'Hello, World!'
--- backend/relationship_route.py
+++ backend/relationship_route.py
@@ -1,7 +1,53 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from api import db1, Relationship, User
 
 relationship = Blueprint('relationship', __name__)
 
 @relationship.route('/helloRelationship')
 def hello():
     return "Hello Relationship"
+
+@relationship.route('/relationship', methods=['GET'])
+def getRelationshipsForEmail():
+    user = getUserByEmail(request.args.get('email'))
+    relationship_list = db1.session.query(Relationship).filter_by(userId=user.id)
+    return jsonify([r.to_json() for r in relationship_list])
+
+@relationship.route('/relationship', methods=['POST'])
+def addRelationshipByEmail():
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            email = data['email']
+            #current_user_info = api.user_info() 
+            user = getUserByEmail(email)
+
+            friend_email = data['friendEmail']
+            friend = getUserByEmail(friend_email)
+            if friend is None:
+                return {"error": f"Friend with email {friend_email} not found"}
+            relationship = Relationship(userId=user.id, friendId=friend.id, relationshipType=data['relationshipType'])
+            db1.session.add(relationship)
+            db1.session.commit()
+            return {"message": f"Relationship between user: {relationship.userId} and userId: {relationship.friendId} has been created successfully."}
+
+        else:
+            return {"error": "The request payload is not in JSON format"}
+
+@relationship.route('/relationship', methods=['DELETE'])
+def deleteRelationshipByEmail():
+    user = getUserByEmail(request.args.get('email'))
+
+    friend_email = request.args.get('friendEmail')
+    friend = getUserByEmail(friend_email)
+    if friend is None:
+        return {"error": f"Friend with email {friend_email} not found"}
+    relationship = Relationship.query.filter(Relationship.userId == user.id).filter(Relationship.friendId == friend.id).first()
+    if relationship is None:
+        return {"error": f"Relationship with {friend_email} not found"} 
+    db1.session.delete(relationship)
+    db1.session.commit() 
+    return {"message": f"Relationship between user: {user.email} and friend: {friend_email} has been deleted successfully."} 
+
+def getUserByEmail(email):
+    return db1.session.query(User).filter_by(email=email).first()
