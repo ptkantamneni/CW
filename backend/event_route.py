3db03fd19e2190ecf97a743464aa4115a707701a
David Nguyen <dnguyen@clari.com>
2020-12-09 14:06:58 -0800

    added event scoring apis

--- backend/event_route.py
+++ backend/event_route.py
@@ -1,6 +1,8 @@
 # CRUD operations for events
 from flask import Blueprint, request, jsonify
-from api import db1 as db, Event
+from api import db1 as db, Event, User
+from datetime import datetime, timedelta
+import event_scoring_route
 
 event = Blueprint('event', __name__, url_prefix = '/event')
 
@@ -64,3 +66,57 @@ def deleteEvent():
 
     return 'SWWE'
     
+@event.route('/getAllEventsForAddress', methods = ['GET'])
+def getAllEventsAtAddressBetweenTime():
+    address = request.args.get('address')
+    check_in_date = request.args.get('checkInDate')
+    check_out_date = request.args.get('checkOutDate')
+    events = getAllEventsHelper(address, check_in_date, check_out_date)
+    for event in events:
+        print(event)
+    return jsonify([e.serialize() for e in events])
+
+@event.route('/updateAllEventsScoreAffectedUser', methods = ['POST'])
+def updateAffectedEvents():
+    user_id = request.args.get('userId')
+    updateEventsForUserWithCovid(user_id)
+
+    return "Updated event scores success"
+
+def getAllEventsHelper(address, check_in_date, check_out_date):
+    events = db.session.query(Event).filter(Event.address == address).filter((Event.checkInDate <= check_out_date) & (Event.checkOutDate >= check_in_date))
+    return events
+
+def incCasesAndGetAllEventsHelper(address, check_in_date, check_out_date):
+    events = db.session.query(Event).filter(Event.address == address).filter((Event.checkInDate <= check_out_date) & (Event.checkOutDate >= check_in_date)).update({'confirmedCases': Event.confirmedCases + 1})
+    print("events updated")
+    db.session.commit()
+
+    eventsList = db.session.query(Event).filter(Event.address == address).filter((Event.checkInDate <= check_out_date) & (Event.checkOutDate >= check_in_date))
+    return eventsList
+
+def getPast2WeekEventsForUserHelper(user_id, date):
+    two_weeks_ago = date - timedelta(days=14)
+    events = db.session.query(Event).filter(Event.createdById == user_id).filter((Event.checkInDate <= date) & (Event.checkOutDate >= two_weeks_ago))
+    return events
+
+def updateEventsForUserWithCovid(user_id):
+    user = db.session.query(User).filter_by(id=user_id).first()
+    if(user.testResult):
+        # if postiive, fetch previous events for user in past 14 weeks
+        user_events = getPast2WeekEventsForUserHelper(user.id, user.testDate)
+        # increment count for each event
+        for event in user_events:
+            print("event: ")
+            print(event.serialize())
+            same_time_events = incCasesAndGetAllEventsHelper(event.address, event.checkInDate, event.checkOutDate)
+            for same_event in same_time_events:
+                print("same_event: ")
+                print(same_event.serialize())
+                # increment and update numCases
+                db.session.commit()
+                event_to_update_score = Event.query.filter_by(id=same_event.id).first()
+
+                new_score = event_scoring_route.calculateScoreForEventId(event_to_update_score.id)
+                Event.query.filter_by(id=same_event.id).update({'riskScore': new_score})
+                db.session.commit()
